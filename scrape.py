c9cdc72adf5e6ba2756da5641cda8978eb6e0d01
David Young <d.young0851@gmail.com>
2021-07-17 15:08:37 +1000

    Updated betfair scraping

--- scrape.py
+++ scrape.py
@@ -7,85 +7,99 @@ import time
 
 
 class BetTypeConversionTable:
-	pass
+    pass
 
 class Event:
-	def __init__(self, link):
-		self.link = link
-		self.bet_odds_map = None
-		self.datetime = None
+    def __init__(self, link):
+        self.link = link
+        self.bet_odds_map = None
+        self.datetime = None
 
 
 class EventMonitor:
-	def __init__(self, events):
-		self.events = events
-		self.bet_pairs = None
+    def __init__(self, events):
+        self.events = events
+        self.bet_pairs = None
 
-		# Outcome Table
-		# Outcome | Bet1 | Bet2 | ...
-		# 
-		self.outcome_table = None
+        # Outcome Table
+        # Outcome | Bet1 | Bet2 | ...
+        # 
+        self.outcome_table = None
 
-		
+        
 
 def betfair_get_vol_map(link, driver=None):
-	rows = driver.find_elements_by_class_name("runner-line")
+    """Get the betting odds and volumes for Betfair horse races."""
 
-	for row in rows:
-		name_elems = row.find_elements_by_class_name("runner-name")
-		if len(name_elems) == 0:
-			print('no names: skipping')
-			continue
-		name = name_elems[0].text
-		odds_box_elems = row.find_elements_by_class_name("runner-fixed-odds")
-		if len(odds_box_elems) == 0:
-			print('no odds: skipping')
-			continue
-		odds_box = odds_box_elems[0]
-		odds = odds_box.find_elements_by_class_name("price-button-odds-price")
-		odds = [float(x.text) for x in odds]
-		print(name, odds)
+    rows = driver.find_elements_by_class_name("runner-line")
+
+    for row in rows:
+        name_elems = row.find_elements_by_class_name("runner-name")
+        if len(name_elems) == 0:
+            print('no names: skipping')
+            continue
+        name = name_elems[0].text
+
+        runner_number = int(row.find_element_by_class_name("saddle-cloth").text)
+
+        cells = row.find_elements_by_class_name("bet-buttons")
+
+        for cell in cells:
+            odds_text = cell.find_element_by_class_name("bet-button-price").text
+            vol_text = cell.find_element_by_class_name("bet-button-size").text
+            print(runner_number, name, odds_text, vol_text)
+
+
 
 
 def ladbrokes_races_get_bet_map(driver, link):
-	
-	rows = driver.find_elements_by_class_name("race-table-row")
+    
+    rows = driver.find_elements_by_class_name("race-table-row")
 
-	bet_map = {}
+    bet_map = {}
 
-	for row in rows:
-		name_elems = row.find_elements_by_class_name("runner-name")
-		if len(name_elems) == 0:
-			print('no names: skipping')
-			continue
-		name = name_elems[0].text
-		odds_box_elems = row.find_elements_by_class_name("runner-fixed-odds")
-		if len(odds_box_elems) == 0:
-			print('no odds: skipping')
-			continue
-		odds_box = odds_box_elems[0]
-		odds = odds_box.find_elements_by_class_name("price-button-odds-price")
-		odds = [float(x.text) for x in odds]
-		print(name, odds)
+    for row in rows:
+        name_elems = row.find_elements_by_class_name("runner-name")
+        if len(name_elems) == 0:
+            print('no names: skipping')
+            continue
+        name = name_elems[0].text
+        odds_box_elems = row.find_elements_by_class_name("runner-fixed-odds")
+        if len(odds_box_elems) == 0:
+            print('no odds: skipping')
+            continue
+        odds_box = odds_box_elems[0]
+        odds = odds_box.find_elements_by_class_name("price-button-odds-price")
+        odds = [float(x.text) for x in odds]
+        print(name, odds)
 
-		bet_map[name] = odds
-
-
+        bet_map[name] = odds
 
 
 
-driver = webdriver.Firefox()
+
+
+driver = webdriver.Chrome()
 driver.implicitly_wait(1)
 
-link = "https://www.ladbrokes.com.au/racing/cranbourne/cd37e2e9-67f1-451a-b800-e278807fc7b6"
+# link = "https://www.ladbrokes.com.au/racing/randwick/b1ee28ef-b6e1-4baf-9e37-fe56d863995c"
 
+# link = "https://www.ladbrokes.com.au/racing/flemington/cd345393-0cb8-4807-a178-70044b1ded04"
+
+# driver.get(link)
+
+# page_name = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "runner-name"))).text
+
+
+# ladbrokes_races_get_bet_map(driver, link)
+
+# driver.close()
+
+link = "https://www.betfair.com.au/exchange/plus/horse-racing/market/1.185315447"
 driver.get(link)
 
 page_name = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "runner-name"))).text
-
-
-ladbrokes_races_get_bet_map(driver, link)
+betfair_get_vol_map(link, driver)
 
 driver.close()
-
 
